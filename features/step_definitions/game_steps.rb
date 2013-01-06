cc6a80ac1aa472a979c07160f93d02ced31b2d38
Hugo Barauna <hugo.barauna@plataformatec.com.br>
2013-01-06 18:47:35 -0200

    Cukes for guess letter with success

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -1,7 +1,20 @@
 # encoding: UTF-8
 
+Dado /^o jogo tem as possíveis palavras para sortear:$/ do |words_table|
+  words = words_table.rows.map(&:last)
+  set_rafflable_words(words)
+end
+
 Dado /^que comecei um jogo$/ do
   start_new_game
+end
+
+Dado /^que escolhi que a palavra a ser sorteada deverá ter "(.*?)"\
+ letras\
+$/ do |number_of_letters|
+  steps %{
+    When I type "#{number_of_letters}"
+  }
 end
 
 Quando /^começo um novo jogo$/ do
@@ -21,11 +34,26 @@ $/ do |number_of_letters|
   }
 end
 
+Quando /^tento adivinhar que a palavra tem a letra "(.*?)"$/ do |letter|
+  steps %{
+    When I type "#{letter}"
+  }
+end
+
 Então /^o jogo termina com a seguinte mensagem na tela:$/ do |text|
   steps %{
     Then it should pass with:
       """
       #{text}
+      """
+  }
+end
+
+Então /^o jogo mostra que eu adivinhei uma letra com sucesso$/ do
+  steps %{
+    Then the stdout should contain:
+      """
+      Você adivinhou uma letra com sucesso.
       """
   }
 end
--- features/support/game_helpers.rb
+++ features/support/game_helpers.rb
@@ -1,7 +1,13 @@
 module NewGameHelpers
+  def set_rafflable_words(words)
+    @raffable_words = words
+  end
+
   def start_new_game
+    set_rafflable_words(%w[hi mom game fruit]) if @raffable_words.nil?
+
     steps %{
-      When I run `forca` interactively
+      When I run `forca "#{@raffable_words}"` interactively
     }
   end
 end
