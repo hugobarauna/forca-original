0d97f7b3fa5122f8553ed23e74cd4e0a321511c6
Hugo Barauna <hugo.barauna@plataformatec.com.br>
2013-01-25 11:24:21 -0200

    Start to control the game with a explicit state variable

--- spec/game_flow_spec.rb
+++ spec/game_flow_spec.rb
@@ -5,7 +5,7 @@ require 'game_flow'
 
 describe GameFlow do
   let(:ui) { double("ui").as_null_object }
-  let(:game) { double("game").as_null_object }
+  let(:game) { double("game", state: :initial).as_null_object }
 
   subject(:game_flow) { GameFlow.new(game, ui) }
 
@@ -18,8 +18,8 @@ describe GameFlow do
     end
   end
 
-  describe "#next step" do
-    context "when the game just started" do
+  describe "#next_step" do
+    context "when the game is in the 'initial' state" do
       it "asks the user for the length of the word to be raffled" do
         question = "Qual o tamanho da palavra a ser sorteada?"
         ui.should_receive(:write).with(question)
@@ -29,42 +29,59 @@ describe GameFlow do
 
         game_flow.next_step
       end
+
+      context "and the user asks to raffle a word" do
+        it "raffles a word with the given length" do
+          word_length = "3"
+          ui.stub(read: word_length)
+
+          game.should_receive(:raffle).with(word_length.to_i)
+
+          game_flow.next_step
+        end
+
+        it "prints a '_' for each letter in the raffled word" do
+          word_length = "3"
+          ui.stub(read: word_length)
+          game.stub(raffle: "mom", raffled_word: "mom")
+
+          ui.should_receive(:write).with("_ _ _")
+
+          game_flow.next_step
+        end
+
+        it "tells if it's not possible to raffle with the given length" do
+          word_length = "20"
+          ui.stub(read: word_length)
+          game.stub(raffle: nil)
+
+          error_message = "Não temos uma palavra com o tamanho " <<
+                         "desejado,\n" <<
+                         "é necessário escolher outro tamanho."
+
+          ui.should_receive(:write).with(error_message)
+
+          game_flow.next_step
+        end
+      end
     end
 
-    context "when the user asks to raffle a word" do
-      it "raffles a word with the given length" do
-        word_length = "3"
-        ui.stub(read: word_length)
+    context "when the game is in the 'word raffled' state" do
+      context "and the user guess a letter with success" do
+        it "prints a success message" do
+          game.stub(state: :word_raffled, guess_letter: true)
 
-        game.should_receive(:raffle).with(word_length.to_i)
+          letter_guess = "f"
+          ui.stub(read: letter_guess)
 
-        game_flow.next_step
-      end
+          success_message = "Você adivinhou uma letra com sucesso."
+          ui.should_receive(:write).with(success_message)
 
-      it "prints a '_' for each letter in the raffled word" do
-        word_length = "3"
-        ui.stub(read: word_length)
-        game.stub(raffle: "mom", raffled_word: "mom")
-
-        ui.should_receive(:write).with("_ _ _")
-
-        game_flow.next_step
-      end
-
-      it "tells if it's not possible to raffle with the given length" do
-        word_length = "20"
-        ui.stub(read: word_length)
-        game.stub(raffle: nil)
-
-        error_message = "Não temos uma palavra com o tamanho " <<
-                       "desejado,\n" <<
-                       "é necessário escolher outro tamanho."
-
-        ui.should_receive(:write).with(error_message)
-
-        game_flow.next_step
+          game_flow.next_step
+        end
       end
     end
+
 
     it "finishes the game when the user asks to" do
       user_input = "fim"
